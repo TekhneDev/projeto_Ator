bda869a046651dba65754b5f5c6975b20d1555ce
carla braga <carlamab2808@outlook.com>
2023-09-26 19:43:00 -0300

    Estrutura do projeto

---
+++ provaA2_27060268_31476309/src/ator/DB/struct.sql
@@ -0,0 +1,19 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  carla && taui
+ * Created: 26 de set. de 2023
+ */
+
+CREATE DATABASE IF NOT EXISTS ator_db;
+
+DROP TABLE IF EXISTS ator;
+
+CREATE TABLE `ator` (
+  `atorID` int NOT NULL AUTO_INCREMENT,
+  `atorNome` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `atorIdade` int DEFAULT NULL,
+  PRIMARY KEY (`atorID`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
